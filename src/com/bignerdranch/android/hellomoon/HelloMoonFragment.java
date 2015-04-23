c47f0c1bb1ca06d731b1847c8b9cffdd50593cdb
temasa <rinaldi.ch@gmail.com>
2015-04-23 10:47:42 +0700

    Ch14-Complete

--- src/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
+++ src/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -13,6 +13,12 @@ public class HelloMoonFragment extends Fragment
 	private AudioPlayer mPlayer = new AudioPlayer();
 	private Button mPlayButton;
 	private Button mStopButton;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState)
